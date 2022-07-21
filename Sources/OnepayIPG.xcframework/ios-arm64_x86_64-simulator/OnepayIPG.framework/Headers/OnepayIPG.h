ab3cd2d0a7b77722026e513b16883e5f67475b3c
Udaya <udaya.kahandawala@spemai.com>
2022-07-21 14:12:24 +0530

    v 1.0.0

---
+++ Sources/OnepayIPG.xcframework/ios-arm64_x86_64-simulator/OnepayIPG.framework/Headers/OnepayIPG.h
@@ -0,0 +1,18 @@
+//
+//  OnepayIPG.h
+//  OnepayIPG
+//
+//  Created by amilla fernando on 2022-07-14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OnepayIPG.
+FOUNDATION_EXPORT double OnepayIPGVersionNumber;
+
+//! Project version string for OnepayIPG.
+FOUNDATION_EXPORT const unsigned char OnepayIPGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnepayIPG/PublicHeader.h>
+
+
